90dd8ba94c03363f2fb81a3f41afa799fd400a3a
rng <rng@stuy.edu>
2019-01-30 22:58:08 -0500

    clickdraw

--- 00_canvas/clickdraw.js
+++ 00_canvas/clickdraw.js
@@ -1,25 +1,63 @@
 var c = document.getElementById("slate");
-
 var ctx = c.getContext("2d");
 
+
+
+var voodoo_mouse_stuff = (canvas, evt) => {
+  var bounds = canvas.getBoundingClientRect();
+  console.log("x: " + (evt.clientX - bounds.left) + "    y: " + (evt.clientY - bounds.top));
+  return { x: evt.clientX - bounds.left, y: evt.clientY - bounds.top };
+}
+
+
+
 var clear_canv = () => {
-    ctx.fillRect(0, 0, c.width, c.height) // top left corner
+  ctx.fillStyle = "#123411"
+  ctx.clearRect(0, 0, c.width, c.height) // top left corner
+  console.log("cleared");
 }
 
 var clear_button = document.getElementById("clear");
 clear_button.addEventListener("click", clear_canv);
 
-function draw() {
-    var canvas = document.getElementById('slate');
-    if (canvas.getContext) {
-        var ctx = canvas.getContext('2d');
 
-        ctx.fillStyle = 'rgb(200, 0, 0)';
-        ctx.fillRect(10, 10, 50, 50);
 
-        ctx.fillStyle = 'rgba(0, 0, 200, 0.5)';
-        ctx.fillRect(30, 30, 50, 50);
-    }
+var toggle_button = document.getElementById("toggle");
+
+var toggle_shape = () => {
+  if (toggle_button.innerHTML == "Toggle Dot") {
+    toggle_button.innerHTML = "Toggle Box";
+  }
+  else if (toggle_button.innerHTML == "Toggle Box") {
+    toggle_button.innerHTML = "Toggle Dot";
+  }
 }
 
-draw()
+toggle_button.addEventListener("click", toggle_shape);
+
+
+
+var current_shape = () => {
+  if (toggle_button.innerHTML == "Toggle Dot") {
+    return("box");
+  }
+  else if (toggle_button.innerHTML == "Toggle Box") {
+    return("dot");
+  }
+}
+
+
+
+var draw_stuff = (mouse_coords, current_shape) => {
+  ctx.fillStyle = "#8a99aa";
+  if (current_shape == "box") {
+    ctx.fillRect(mouse_coords.x - 10, mouse_coords.y - 10, 20, 20);
+  }
+  else {
+    ctx.beginPath();
+    ctx.arc(mouse_coords.x, mouse_coords.y, 10, 0, 2 * Math.PI);
+    ctx.fill();
+  }
+}
+
+c.addEventListener("click", function(evt) { draw_stuff(voodoo_mouse_stuff(c, evt), current_shape()); });
