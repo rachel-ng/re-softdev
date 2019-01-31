310cdc34089724f5169f885eb8d1090df05b3a1b
rng <rng@stuy.edu>
2019-01-31 18:26:59 -0500

    shorter js code atm

---
+++ 01_canvas/clickdraw.js
@@ -0,0 +1,48 @@
+// set variables
+var c = document.getElementById("slate");
+var current_shape = "box";
+
+// acknowledge buttons
+var clear_button = document.getElementById("clear");
+var toggle_button = document.getElementById("toggle");
+
+// instantiate context
+var ctx = c.getContext("2d");
+
+
+
+// clear button
+clear_button.addEventListener("click", function(e) {
+  ctx.clearRect(0, 0, c.width, c.height)
+  console.log("cleared");
+});
+
+
+
+// toggle button
+toggle_button.addEventListener("click", function(e) {
+  if (current_shape == "box") {
+    toggle_button.innerHTML = "Toggle Box"; // not necessary, changes button text
+    current_shape = "dot";
+  }
+  else if (current_shape == "dot") {
+    toggle_button.innerHTML = "Toggle Dot"; // not necessary, changes button text
+    current_shape = "box";
+  }
+});
+
+
+
+// event listener so it actually draws
+c.addEventListener("click", function(e) {
+  ctx.fillStyle = "#8a99aa";
+  if (current_shape == "box") {
+    ctx.fillRect(e.offsetX, e.offsetY, 20, 20);
+    // offsetX and offsetY are the coordinates of your mouse relative to the thing being spied on
+  }
+  else if (current_shape == "dot") {
+    ctx.beginPath();
+    ctx.arc(e.offsetX, e.offsetY, 10, 0, 2 * Math.PI);
+    ctx.fill();
+  }
+});
