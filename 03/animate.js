01670ab0c10aef04875581c4974f3add16ffeeca
rng <rng@stuy.edu>
2019-02-04 14:36:07 -0500

    animate.js

--- 03/animate.js
+++ 03/animate.js
@@ -5,24 +5,36 @@ var animate = document.getElementById("circle");
 var stop = document.getElementById("stop");
 
 var requestID;
-var radiusID = 50;
+var radiusID = 0;
 var growing = true;
 
 
+
 var drawDot = function() {
-    if (growing) {
-	radiusID += 1; 
-    }
-    ctx.beginPath();
-    ctx.arc(c.width / 2, c.height / 2, radiusID, 0, 2 * Math.PI);
-    ctx.stroke();
-    ctx.fill();
+  if (growing && radiusID + c.width / 2 < c.width) {
+    radiusID += 1;
+  }
+  else if (radiusID + c.width / 2 == c.width){
+    radiusID -= 1;
+    growing = false;
+  }
+  else if (radiusID == 0) {
+    growing = true;
+  }
+  else if (!growing) {
+    ctx.clearRect(0, 0, c.width, c.height)
+    radiusID -= 1;
+  }
+
+  ctx.beginPath();
+  ctx.arc(c.width / 2, c.height / 2, radiusID, 0, 2 * Math.PI);
+  ctx.stroke();
+  ctx.fill();
+
+  requestID = requestAnimationFrame(drawDot);
 }
 
 
 
-cancelAnimationFrame(requestID)
-
-stop.addEventListener("click", function(e){growing = false;});
-animate.addEventListener("click", function(e) {requestID = requestAnimationFrame(drawDot);
-});//growing = true);
+stop.addEventListener("click", function(e) {cancelAnimationFrame(requestID);});
+animate.addEventListener("click", drawDot);
