cf5a978488830d01381636517155ee6f578488d2
rng <rng@stuy.edu>
2019-02-04 03:20:25 -0500

    yay

--- 02_canvas/dotconn.js
+++ 02_canvas/dotconn.js
@@ -1,21 +1,10 @@
-// Team Existential Crisis -- Sajed Nahian and Rachel Ng
-// SoftDev2 pd7
-// K01 -- ...and I want to Paint It Better
-// 2019-01-31
-
-
-// set variables
 var c = document.getElementById("slate");
-
-// acknowledge buttons
-var clear_button = document.getElementById("clear");
-
-// instantiate context
 var ctx = c.getContext("2d");
 
+var clear_button = document.getElementById("clear");
 
 
-// clear button
+
 clear_button.addEventListener("click", function(e) {
   ctx.clearRect(0, 0, c.width, c.height)
   console.log("cleared");
@@ -23,16 +12,29 @@ clear_button.addEventListener("click", function(e) {
 
 
 
-// event listener so it actually draws
+var p_x = -1000
+var p_y = -1000;
+var circle = true;
+
 c.addEventListener("click", function(e) {
-    ctx.fillStyle = "#ff0000";
-    ctx.beginPath(); // empties list of paths and creates a new one
+  ctx.fillStyle = "#8a99aa";
+  if (circle) {
+    ctx.beginPath();
     ctx.arc(e.offsetX, e.offsetY, 10, 0, 2 * Math.PI);
-    //ctx.closePath()
-    
+    ctx.stroke();
+    ctx.fill();
+    circle = false;
+  }
+  else if (!circle) {
+    ctx.beginPath();
+    ctx.arc(e.offsetX, e.offsetY, 10, 0, 2 * Math.PI);
+    ctx.stroke();
+    ctx.fill();
+    ctx.beginPath();
+    ctx.moveTo(p_x, p_y);
     ctx.lineTo(e.offsetX, e.offsetY);
     ctx.stroke();
-    ctx.closePath();
-    ctx.moveTo(e.offsetX, e.offsetY);
-    //ctx.fill();
+  }
+  p_x = e.offsetX;
+  p_y = e.offsetY;
 });
