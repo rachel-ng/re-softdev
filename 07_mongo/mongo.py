0387a47f87fafc5608f809cc9b891504fc7ee45e
rachel-ng <rng@stuy.edu>
2019-03-05 01:37:55 -0500

    mongo

--- 07_mongo/mongo.py
+++ 07_mongo/mongo.py
@@ -1,46 +1,52 @@
+# Existential Ramen -- Sajed Nahian, Rachel Ng
+# SoftDev2 pd7
+# K07 -- Import/Export Bank
+# 2019-03-04
+
+'''
+
+Rick and Morty Characters (first 100) from the Rick and Morty API
+https://rickandmortyapi.com/api/character/
+https://raw.githubusercontent.com/rachel-ng/re-softdev/master/07_mongo/szechuansauce.json
+
+contents of "results" from the first 5 pages
+(everything between the brackets, with added commas between
+last and first characters of each new page)
+must be inside new brackets for import as a 'json array' by mongo
+
+mongoimport --jsonArray --db existentialramen --collection rickandmorty --drop --file re-softdev/07_mongo/szechuansauce.json
+
+
+'''
+
 import pymongo
 SERVER_ADDR = "167.99.13.106"
 connection = pymongo.MongoClient(SERVER_ADDR)
 db = connection.existentialramen
-collection = db.blackmirror
-#
-#
-#
-#
-#
+collection = db.rickandmorty
 
-
-def restaurant_borough(boro):
-    results = collection.find({"borough":boro})
+def character_name(name):
+    results = collection.find({"name":name})
     for res in results:
-       print(res['name'])
-       print(res['address']['building'] + " " + res['address']['street'])
-       print()
+        print(res['name'] + " (" + res['status'] + ")\n")
 
-def r_borough (borough):
-    return [restaurant for restaurant in db.restaurants.find({"borough": borough})]
+def character_status(status):
+    results = collection.find({"status":status})
+    for res in results:
+        print(res['name'] + " (" + res['status'] + ")\n")
 
-def r_zip (zipcode):
-    return [restaurant for restaurant in db.restaurants.find({"address.zipcode": zipcode})]
+def character_species(species):
+    results = collection.find({"species":species})
+    for res in results:
+        print(res['name'] + " (" + res['status'] + ")\n" + res['species'] + "\n")
 
-def r_zip_grade (zipcode, grade):
-    return [restaurant for restaurant in db.restaurants.find({"address.zipcode":zipcode},{"grades.grade": grade})]
+def character_origin(origin):
+    results = collection.find({"origin.name":origin})
+    for res in results:
+        print(res['name'] + " (" + res['status'] + ")\n" + res['origin.name'] + "\n")
 
-def r_zip_thresh (zipcode, thresh):
-    return [restaurant for restaurant in db.restaurants.find({"address.zip":zipcode},{"grades.score": {"$lt": thresh}})]
 
-print("borough")
-for i in r_borough("Queens"):
-    print (str(i["_id"]) + "\t" + i["name"])
-
-print("\n\n\n\n\nzip")
-for i in r_zip("11355"):
-    print (str(i["_id"]) + "\t" + i["name"])
-
-restaurant_borough("Queens")
-
-#All restaurants in a specified borough.
-#All restaurants in a specified zip code.
-#All restaurants in a specified zip code and with a specified grade.
-#All restaurants in a specified zip code with a score below a specified threshold.
-#Something more clever.
+def character_location(location):
+    results = collection.find({"location.name":location})
+    for res in results:
+        print(res['name'] + " (" + res['status'] + ")\n" + res['location.name'] + "\n")
