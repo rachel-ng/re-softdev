3a6adfb4656af28836b255c63a53554a3b42a2e4
rachel-ng <rng@stuy.edu>
2019-04-16 18:30:41 -0400

    pyth.py

--- 18_listcomp/pyth.py
+++ 18_listcomp/pyth.py
@@ -1,7 +1,9 @@
 import sys, math
 
-print([(i, x, int(math.sqrt(i**2 + x**2)))
-       for x in range(0, int(sys.argv[1])+ 1)
-       for i in range(int(sys.argv[1])+ 1)
-       if (math.sqrt(i**2 + x**2) < int(sys.argv[1])+ 1) and
-       math.sqrt(i**2 + x**2) == int(math.sqrt(i**2 + x**2))])
+def pyth (n):
+    return [(x, i, int(math.sqrt(i**2 + x**2))) for x in range(1, n + 1) for i in range(x, n + 1)
+            if math.sqrt(i**2 + x**2) <= n and math.sqrt(i**2 + x**2) == int(math.sqrt(i**2 + x**2))]
+
+print(pyth(5))
+print(pyth(12))
+print(pyth(15))
