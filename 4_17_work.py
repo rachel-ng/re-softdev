2af039ac32bb870ab0c9009723bb3d649fee1ad5
rachel-ng <rng@stuy.edu>
2019-04-17 12:45:41 -0400

    work

--- 4_17_work.py
+++ 4_17_work.py
@@ -11,3 +11,5 @@ i_file = open("2591-0.txt", "r+")
 grimm = [line.strip("\r\n ") for line in i_file if line != "\r\n"]
 print([i for i in reduce((lambda x, y: x + y), grimm).split(" ") if i != ''])
 
+def freq_word(word):
+    
