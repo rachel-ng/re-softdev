db798806729387a7035779a1c213faf066e1192a
rachel-ng <rng@stuy.edu>
2019-04-18 17:08:43 +0000

    .

--- 20_anon-reduce/reduce.py
+++ 20_anon-reduce/reduce.py
@@ -10,7 +10,7 @@ reduce((lambda x, y: x * y), [1,2,3,4]) # = 24
 #total frequency group of words
 #most frequently occuring word
 
-i_file = open("2591-0.txt", "r+")
+i_file = open("i.txt", "r+")
 grimmm = [line.replace("\r\n"," ").replace(". "," ").replace("\xe2\x80\x99", "") for line in i_file if line != "\r\n"]
 grimm = [i for i in reduce((lambda x, y: x + y), grimmm).split(" ") if i != '']
 
@@ -24,8 +24,8 @@ all_words = [i for i in set(grimm)]
 #print(all_words)
 
 
-highest_freq = dict([[freq_word(i),i] for i in all_words if freq_word(i) > 10])
-highest_frequency = highest_freq.keys()[:]
+highest_freq = dict([[freq_word(i),i] for i in all_words if freq_word(i) > 1])
+print(highest_freq.keys())
 
-#print(highest_frequency.max())
-print(highest_freq[highest_frequency.max()])
+print(max(highest_freq.keys()))
+print(highest_freq[max(highest_freq.keys())])
