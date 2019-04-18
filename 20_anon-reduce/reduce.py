33343f544e061dc65406f8872c332e1190173449
rachel-ng <rng@stuy.edu>
2019-04-18 12:59:45 -0400

    sos my laptop is dying

--- 20_anon-reduce/reduce.py
+++ 20_anon-reduce/reduce.py
@@ -1,3 +1,6 @@
+# Rthur N TW
+
+
 from functools import reduce
 
 # similar structure lisp
@@ -8,7 +11,21 @@ reduce((lambda x, y: x * y), [1,2,3,4]) # = 24
 #most frequently occuring word
 
 i_file = open("2591-0.txt", "r+")
-grimm = [line.replace("\r\n"," ") for line in i_file if line != "\r\n"]
-grimm = [i for i in reduce((lambda x, y: x + y), grimm).split(" ") if i != '']
+grimmm = [line.replace("\r\n"," ").replace(". "," ").replace("\xe2\x80\x99", "") for line in i_file if line != "\r\n"]
+grimm = [i for i in reduce((lambda x, y: x + y), grimmm).split(" ") if i != '']
 
-print(grimm)
+def freq_word(word):
+    return reduce((lambda x, y: x + 1),[1 for i in grimm if i.lower() == word.lower()])
+
+#print(freq_word("cat"))
+
+all_words = [i for i in set(grimm)]
+
+#print(all_words)
+
+
+highest_freq = dict([[freq_word(i),i] for i in all_words if freq_word(i) > 10])
+highest_frequency = highest_freq.keys()[:]
+
+#print(highest_frequency.max())
+print(highest_freq[highest_frequency.max()])
